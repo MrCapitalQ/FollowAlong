cdec036acf2edd5e7a83fcd5b875248b40bbff18
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2023-12-11 20:03:09 -0500

    Initial generic host implementation.

---
+++ src/MrCapitalQ.FollowAlong/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MrCapitalQ.FollowAlong;
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<WinUIHostedService<App>>();
+        services.AddSingleton<App>();
+        services.AddTransient<MainWindow>();
+    })
+    .Build().Run();
---
+++ src/MrCapitalQ.FollowAlong/WinUIHostedService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MrCapitalQ.FollowAlong
+{
+    internal sealed class WinUIHostedService<TApplication> : IHostedService, IDisposable
+        where TApplication : Application
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly IServiceProvider _serviceProvider;
+
+        public WinUIHostedService(IHostApplicationLifetime hostApplicationLifetime, IServiceProvider serviceProvider)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var thread = new Thread(Main);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void Main()
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            Application.Start(p =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _serviceProvider.GetRequiredService<TApplication>();
+            });
+            _hostApplicationLifetime.StopApplication();
+        }
+    }
+}
