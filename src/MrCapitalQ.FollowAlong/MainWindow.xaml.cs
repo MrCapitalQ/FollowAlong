828146928b12894fb8bdcd1c5eab428fc08473af
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-01-02 14:01:20 -0500

    Reenable showing app in switchers so it can be a sharable window.

--- src/MrCapitalQ.FollowAlong/MainWindow.xaml.cs
+++ src/MrCapitalQ.FollowAlong/MainWindow.xaml.cs
@@ -52,7 +52,6 @@ namespace MrCapitalQ.FollowAlong
 
             AppWindow.ResizeClient(new SizeInt32(1280, 720));
             AppWindow.Move(new PointInt32((int)monitor.ScreenSize.X - 1, (int)monitor.ScreenSize.Y - 1));
-            AppWindow.IsShownInSwitchers = false;
             this.SetIsResizable(false);
             this.SetIsMinimizable(false);
             this.SetIsMaximizable(false);
@@ -66,7 +65,6 @@ namespace MrCapitalQ.FollowAlong
             MainContent.Visibility = Visibility.Visible;
 
             AppWindow.Resize(new SizeInt32(640, 480));
-            AppWindow.IsShownInSwitchers = true;
             this.CenterOnScreen();
             this.SetIsResizable(true);
             this.SetIsMinimizable(true);
