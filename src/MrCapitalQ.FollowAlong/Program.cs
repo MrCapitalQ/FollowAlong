7423b6cf7007b2805f117de2aae3266971e590f2
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-01-03 12:40:10 -0500

    Enforce single instance of app.

--- src/MrCapitalQ.FollowAlong/Program.cs
+++ src/MrCapitalQ.FollowAlong/Program.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Windows.AppLifecycle;
 using MrCapitalQ.FollowAlong;
 using MrCapitalQ.FollowAlong.Core.Capture;
 using MrCapitalQ.FollowAlong.Core.HotKeys;
 using MrCapitalQ.FollowAlong.Core.Monitors;
 using MrCapitalQ.FollowAlong.Core.Tracking;
+using System;
+
+var keyInstance = AppInstance.FindOrRegisterForKey("f3c5d6b4-6a0e-4e7c-9c1d-2a4d8d8a3d4e");
+if (!keyInstance.IsCurrent)
+{
+    var activationArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+    await keyInstance.RedirectActivationToAsync(activationArgs);
+    return;
+}
 
 Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
@@ -19,4 +29,5 @@ Host.CreateDefaultBuilder(args)
         services.AddTransient<TrackingTransformService>();
         services.AddSingleton<HotKeysService>();
     })
-    .Build().Run();
+    .Build()
+    .Run();
