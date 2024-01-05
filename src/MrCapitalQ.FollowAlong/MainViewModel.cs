cc61edb60b99eb1e8eb6644906b987ce06826af9
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-01-04 19:59:42 -0500

    Display a monitor graphic for each available display.

--- src/MrCapitalQ.FollowAlong/MainViewModel.cs
+++ src/MrCapitalQ.FollowAlong/MainViewModel.cs
@@ -12,10 +12,10 @@ namespace MrCapitalQ.FollowAlong
         private readonly BitmapCaptureService _captureService;
 
         [ObservableProperty]
-        private ObservableCollection<MonitorInfo> _monitors = new();
+        private ObservableCollection<MonitorViewModel> _monitors = new();
 
         [ObservableProperty]
-        private MonitorInfo? _selectedMonitor;
+        private MonitorViewModel? _selectedMonitor;
 
         public MainViewModel(MonitorService monitorService,
             HotKeysService hotKeysService,
@@ -23,8 +23,8 @@ namespace MrCapitalQ.FollowAlong
         {
             hotKeysService.HotKeyInvoked += HotKeysService_HotKeyInvoked;
 
-            Monitors = new(monitorService.GetAll());
-            SelectedMonitor = Monitors.FirstOrDefault(x => x.IsPrimary);
+            Monitors = new(monitorService.GetAll().Select(x => new MonitorViewModel(x)));
+            SelectedMonitor = Monitors.FirstOrDefault(x => x.MonitorInfo.IsPrimary);
             _captureService = captureService;
         }
 
@@ -33,13 +33,24 @@ namespace MrCapitalQ.FollowAlong
             if (e.HotKeyType != HotKeyType.StartStop)
                 return;
 
-                if (!_captureService.IsStarted && SelectedMonitor is not null)
-                {
-                    var captureItem = SelectedMonitor.CreateCaptureItem();
-                    _captureService.StartCapture(captureItem);
-                }
-                else if (_captureService.IsStarted)
-                    _captureService.StopCapture();
+            if (!_captureService.IsStarted && SelectedMonitor is not null)
+            {
+                var captureItem = SelectedMonitor.MonitorInfo.CreateCaptureItem();
+                _captureService.StartCapture(captureItem);
             }
+            else if (_captureService.IsStarted)
+                _captureService.StopCapture();
+        }
+    }
+
+    internal class MonitorViewModel
+    {
+        public MonitorViewModel(MonitorInfo monitorInfo)
+        {
+            MonitorInfo = monitorInfo;
+        }
+
+        public MonitorInfo MonitorInfo { get; }
+        public double AspectRatio => MonitorInfo.ScreenSize.X / MonitorInfo.ScreenSize.Y;
     }
 }
