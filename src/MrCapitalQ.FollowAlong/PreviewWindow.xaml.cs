52a4bbbab48cbffa282b8700c6b7c308f7150bae
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-01-08 16:55:50 -0500

    Enable preview in preview window.

--- src/MrCapitalQ.FollowAlong/PreviewWindow.xaml.cs
+++ src/MrCapitalQ.FollowAlong/PreviewWindow.xaml.cs
@@ -1,5 +1,9 @@
 using Microsoft.UI.Xaml;
+using MrCapitalQ.FollowAlong.Core.Capture;
 using MrCapitalQ.FollowAlong.Core.Monitors;
+using MrCapitalQ.FollowAlong.Core.Tracking;
+using System;
+using System.Runtime.InteropServices;
 using Windows.Graphics;
 using WinUIEx;
 
@@ -7,12 +11,22 @@ namespace MrCapitalQ.FollowAlong
 {
     public sealed partial class PreviewWindow : Window
     {
+        private const int BottomPadding = 48;
         private readonly static SizeInt32 s_windowSize = new(480, 320);
+        private readonly TrackingTransformService _trackingTransformService;
 
-        public PreviewWindow()
+        public PreviewWindow(BitmapCaptureService captureService,
+            TrackingTransformService trackingTransformService)
         {
             InitializeComponent();
 
+            captureService.RegisterFrameHandler(Preview);
+
+            _trackingTransformService = trackingTransformService;
+            _trackingTransformService.Zoom = 1.5;
+            _trackingTransformService.StartTrackingTransforms(Preview);
+
+            ExcludeWindowFromCapture();
             this.SetIsShownInSwitchers(false);
             this.SetIsResizable(false);
             this.SetIsMinimizable(false);
@@ -21,10 +35,34 @@ namespace MrCapitalQ.FollowAlong
 
             ExtendsContentIntoTitleBar = true;
             AppWindow.Resize(s_windowSize);
+            RepositionToPreviewPosition();
 
+            Root.Loaded += Root_Loaded;
+            Activated += PreviewWindow_Activated;
+        }
+
+        private void RepositionToPreviewPosition()
+        {
             var appMonitor = this.GetWindowMonitorSize();
             if (appMonitor is not null)
-                AppWindow.Move(new PointInt32((int)appMonitor.ScreenSize.X - s_windowSize.Width, (int)appMonitor.ScreenSize.Y - s_windowSize.Height));
+                AppWindow.Move(new PointInt32(0,
+                    (int)(appMonitor.ScreenSize.Y - s_windowSize.Height - (BottomPadding * Root.XamlRoot?.RasterizationScale ?? 1))));
+        }
+
+        private void Root_Loaded(object sender, RoutedEventArgs e) => RepositionToPreviewPosition();
+
+        private void PreviewWindow_Activated(object sender, WindowActivatedEventArgs args)
+            => _trackingTransformService.UpdateLayout();
+
+        [DllImport("user32.dll")]
+        private static extern uint SetWindowDisplayAffinity(IntPtr hwnd, uint dwAffinity);
+
+        private void ExcludeWindowFromCapture()
+        {
+            const uint WDA_EXCLUDEFROMCAPTURE = 0x00000011;
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            _ = SetWindowDisplayAffinity(hWnd, WDA_EXCLUDEFROMCAPTURE);
         }
     }
 }
