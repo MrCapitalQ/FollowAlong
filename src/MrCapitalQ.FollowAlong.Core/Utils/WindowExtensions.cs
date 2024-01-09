c0ff483ba6972396dbc2a89ddfdb391236e5df13
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-01-08 20:24:18 -0500

    Add cleanup to preview window.

---
+++ src/MrCapitalQ.FollowAlong.Core/Utils/WindowExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MrCapitalQ.FollowAlong.Core.Utils
+{
+    public static class WindowExtensions
+    {
+        const uint WDA_EXCLUDEFROMCAPTURE = 0x00000011;
+
+        [DllImport("user32.dll")]
+        private static extern uint SetWindowDisplayAffinity(IntPtr hwnd, uint dwAffinity);
+
+        public static void ExcludeFromCapture(this Window window)
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            _ = SetWindowDisplayAffinity(hWnd, WDA_EXCLUDEFROMCAPTURE);
+        }
+    }
+}
