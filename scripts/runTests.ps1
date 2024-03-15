d689563b6429a1eadd887c3c4e00933d5666a29a
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-03-15 08:31:56 -0400

    Update runTests script to ignore generated code files from coverage metrics.

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -8,7 +8,11 @@ try {
     Get-ChildItem -Path . -Recurse | Where-Object { $_.Name -ilike "TestResults" } | Remove-Item -Force -Recurse
 
     # EnableMsixTooling MSBuild property is needed to build tests from dotnet CLI
-    dotnet test --collect:"XPlat Code Coverage" /p:EnableMsixTooling=true /p:Platform=x64
+    dotnet test `
+        --collect:"XPlat Code Coverage" `
+        /p:EnableMsixTooling=true `
+        /p:Platform=x64 `
+        -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.ExcludeByFile="**/*.g.cs,**/*.g.i.cs"
     if (!$?) { Exit $LASTEXITCODE }
 
     reportgenerator -reports:"./test/**/coverage.cobertura.xml" -targetdir:"./TestResults/CoverageReport" -reporttypes:Html
