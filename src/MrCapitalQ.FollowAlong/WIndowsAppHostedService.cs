bb03b32bef64dcf634d57f5091c612df10bcf82d
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-04-01 10:49:27 -0400

    Update startup to latest pattern. (#57)

---
+++ src/MrCapitalQ.FollowAlong/WIndowsAppHostedService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MrCapitalQ.FollowAlong;
+
+internal sealed class WindowsAppHostedService<TApplication> : IHostedService
+    where TApplication : Application
+{
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly IServiceProvider _serviceProvider;
+
+    public WindowsAppHostedService(IHostApplicationLifetime hostApplicationLifetime, IServiceProvider serviceProvider)
+    {
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var thread = new Thread(Main);
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    private void Main()
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start(p =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _serviceProvider.GetRequiredService<TApplication>();
+        });
+        _hostApplicationLifetime.StopApplication();
+    }
+}
